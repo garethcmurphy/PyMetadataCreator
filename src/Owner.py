ab692f8823f082090e3b09f9a6f994e9a079fae2
garethcmurphy <garethcmurphy@gmail.com>
2018-06-08 01:38:14 +0200

    Fix indentation issue 🐛

--- src/Owner.py
+++ src/Owner.py
@@ -2,9 +2,17 @@
 class Owner:
     def __init__(self):
         self.x = 2
-        self.name = "string"
+        self.owner_name = "string"
         self.email = "string@string.com"
         self.owner_list = {}
 
-    def setup(self):
-        self.name = "string"
+    def setup(self, name, email, instrument):
+        self.owner_name = "string"
+        owner = {
+            "name": name,
+            "email": email
+        }
+        self.owner_list[instrument] = owner
+
+    def loop(self):
+        self.setup("Joe Bloggs", "joe.bloggs@esss.se", "MultiBlade")

bb100a307e44eb1b6df63e00b57b3a95c4972fff
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-07 10:35:48 +0200

    rename files

--- dataset_proc.py
+++ dataset_proc.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 import os
 import json
-mydir = "./"
+mydir = "./data"
 
 data = {}
+i = 0
 
 for dirpath, dirnames, filenames in os.walk(mydir):
     if not dirnames:
         print (dirpath, "has 0 subdirectories and", len(filenames), "files")
         savedPath = os.getcwd()
         print (filenames)
+        i= i+1
+        my_orig = {
+            'filenames' : filenames
+        }
+        data['orig'+str(i)] = my_orig
 
-
-
-data['orig'] = filenames
 json_data = json.dumps(data)
 print(json_data)
 

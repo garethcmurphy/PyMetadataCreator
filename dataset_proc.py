3a9680c072b8b538730c872a7aa03089f5aa7e8d
garethcmurphy <garethcmurphy@gmail.com>
2018-06-07 18:22:15 +0200

    PEP8 tidyup 🚧

--- dataset_proc.py
+++ dataset_proc.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import os
 import json
+import os
 
 
 class PyDatasetProcessor:
     def __init__(self):
-        self.mydir ="./data"
+        self.mydir = "./data"
         pass
 
     def walk_tree(self):
@@ -16,47 +16,46 @@ class PyDatasetProcessor:
 
         for dirpath, dirnames, filenames in os.walk(self.mydir):
             if not dirnames:
-                print (dirpath, "has 0 subdirectories and", len(filenames), "files")
-                savedPath = os.getcwd()
-                print (filenames)
-                i= i+1
+                print(dirpath, "has 0 subdirectories and", len(filenames), "files")
+                print(filenames)
+                i = i + 1
                 my_dataset = {
-                  "principalInvestigator": "string",
-                  "endTime": "2018-06-07T09:46:27.560Z",
-                  "creationLocation": "string",
-                  "dataFormat": "lst",
-                  "scientificMetadata": {},
-                  "owner": "Francesco Piscitelli",
-                  "ownerEmail": "francesco.piscitelli@esss.se",
-                  "orcidOfOwner": "0000-0002-0325-4407",
-                  "contactEmail": "francesco.piscitelli@esss.se",
-                  "sourceFolder": "string",
-                  "size": 102400,
-                  "packedSize": 1024000,
-                  "creationTime": "2018-06-07T08:46:19.611Z",
-                  "type": "string",
-                  "validationStatus": "string",
-                  "keywords": [
-                    "string"
-                  ],
-                  "description": "string",
-                  "userTargetLocation": "string",
-                  "classification": "string",
-                  "license": "ESS",
-                  "version": "string",
-                  "doi": "string",
-                  "isPublished": True,
-                  "ownerGroup": "string",
-                  "accessGroups": [
-                    "string"
-                  ],
-                  "createdBy": "string",
-                  "updatedBy": "string",
-                  "createdAt": "2018-06-07T08:46:19.611Z",
-                  "updatedAt": "2018-06-07T08:46:19.611Z"
+                    "principalInvestigator": "string",
+                    "endTime": "2018-06-07T09:46:27.560Z",
+                    "creationLocation": "string",
+                    "dataFormat": "lst",
+                    "scientificMetadata": {},
+                    "owner": "Francesco Piscitelli",
+                    "ownerEmail": "francesco.piscitelli@esss.se",
+                    "orcidOfOwner": "0000-0002-0325-4407",
+                    "contactEmail": "francesco.piscitelli@esss.se",
+                    "sourceFolder": "string",
+                    "size": 102400,
+                    "packedSize": 1024000,
+                    "creationTime": "2018-06-07T08:46:19.611Z",
+                    "type": "string",
+                    "validationStatus": "string",
+                    "keywords": [
+                        "string"
+                    ],
+                    "description": "string",
+                    "userTargetLocation": "string",
+                    "classification": "string",
+                    "license": "ESS",
+                    "version": "string",
+                    "doi": "string",
+                    "isPublished": True,
+                    "ownerGroup": "string",
+                    "accessGroups": [
+                        "string"
+                    ],
+                    "createdBy": "string",
+                    "updatedBy": "string",
+                    "createdAt": "2018-06-07T08:46:19.611Z",
+                    "updatedAt": "2018-06-07T08:46:19.611Z"
                 }
                 my_orig = {
-                    "datafileList" : filenames,
+                    "datafileList": filenames,
                     "createdBy": "string",
                     "updatedBy": "ingestor",
                     "datasetId": "<PID>/a4ec147c-0d5d-4cfd-9fa5-893423a68729",
@@ -65,8 +64,8 @@ class PyDatasetProcessor:
                     "createdAt": "2018-04-23T09:23:47.918Z",
                     "updatedAt": "2018-04-23T09:59:04.506Z"
                 }
-                orig_data ["orig"+str(i)] = ( my_orig)
-                datasets ["orig"+str(i)] = ( my_dataset)
+                orig_data["orig" + str(i)] = my_orig
+                datasets["orig" + str(i)] = my_dataset
 
         json_orig_data = json.dumps(orig_data)
         print(json_orig_data)
@@ -74,12 +73,12 @@ class PyDatasetProcessor:
         print(json_datasets)
 
         with open('datasets.json', 'w') as f:
-          json.dump(datasets, f, ensure_ascii=False)
+            json.dump(datasets, f, ensure_ascii=False)
 
         with open('orig.json', 'w') as f:
-          json.dump(orig_data, f, ensure_ascii=False)
+            json.dump(orig_data, f, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    g = PyDatasetProcessor( )
+    g = PyDatasetProcessor()
     g.walk_tree()

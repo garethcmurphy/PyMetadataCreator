125110478b222e0c460bab26b5f7d3adc805a4da
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-11 13:59:04 +0200

    📚

---
+++ src/orig.py
@@ -0,0 +1,17 @@
+class Orig:
+    def __init__(self):
+        self.orig = {
+            "size": 20,
+            "dataFileList": "test",
+            "ownerGroup": "brightness",
+            "accessGroups": [
+                "brightness"
+            ],
+            "createdBy": "ingestor",
+            "updatedBy": "ingestor",
+            "datasetId": "10.17199/" ,
+            "rawDatasetId": "string",
+            "derivedDatasetId": "string",
+            "createdAt": "2018-04-23T09:23:47.918Z",
+            "updatedAt": "2018-04-23T09:59:04.506Z"
+        }

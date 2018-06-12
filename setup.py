89e13f414809bd98cd06af829d4f37e7294c2f60
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-12 06:30:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Metadata Creator',
+      author='Gareth Murphy',
+      author_email='garethcmurphy@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )

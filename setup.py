4a6c28b81374eb4d21eb039cce3693b45dd5b475
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-12 06:31:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='Distutils',
+setup(name='PyMetadataCreator',
       version='1.0',
       description='Python Metadata Creator',
       author='Gareth Murphy',

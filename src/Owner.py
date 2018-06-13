c60cb7a6a78dac3e793e3102bbab1f483e8b0ebc
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-13 10:06:54 +0200

    🐛

--- src/Owner.py
+++ src/Owner.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
+import sys
+import json
+
+
 class Owner:
     def __init__(self):
-        self.x = 2
         self.owner_name = "string"
         self.email = "string@string.com"
         self.owner_list = {}
 
-    def setup(self, name, email, instrument):
+    def setup(self, name, email, orcid, instrument):
         self.owner_name = "string"
         owner = {
             "name": name,
-            "email": email
+            "email": email,
+            "orcid": orcid,
         }
         self.owner_list[instrument] = owner
 
     def loop(self):
-        self.setup("Joe Bloggs", "joe.bloggs@esss.se", "MultiBlade")
+        self.setup("Joe Smith", "joe.smith@esss.se", "0002", "MultiBlade")
+        json.dump(self.owner_list, sys.stdout, indent=2)
+
+        with open('owner.json', 'w') as f:
+            json.dump(self.owner_list, f, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+    o = Owner()
+    o.loop()

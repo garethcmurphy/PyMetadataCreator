df17cc58bf077294d23c7be9cb45afbc14e80a0e
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-13 10:06:54 +0200

    📇

--- src/PyMetadataCreator.py
+++ src/PyMetadataCreator.py
@@ -13,8 +13,6 @@ from orig import Orig
 class PyDatasetProcessor:
     def __init__(self):
         self.mydir = "./data"
-        #self.mydir = "static"
-        # self.mydir = "/users/detector/experiments/multiblade/data/brightness"
         self.year_month_regex = '20[0-9]{2}_[0-1][0-9]'
         self.hostname = socket.gethostname()
         if self.hostname == 'login.esss.dk':
@@ -41,7 +39,7 @@ class PyDatasetProcessor:
                 file_list = []
                 total_file_size = 0
 
-                filenum =0
+                filenum = 0
                 for file in filenames:
                     longname = dirpath + '/' + file
                     filenum += 1

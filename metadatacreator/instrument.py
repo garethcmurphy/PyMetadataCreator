71bb4f0d1603f1b5451b5aabb93553ec0755557a
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-27 11:52:49 +0200

    📚

--- metadatacreator/instrument.py
+++ metadatacreator/instrument.py
@@ -1,69 +1,91 @@
 #!/usr/bin/env python3
 class Instrument:
 
-    def __init__(self):
-        self.inst = {'owner': "ESS",
-                     'ownerEmail': "undefined@esss.se",
-                     'orcidOfOwner': "0000-0000-0000-0000",
-                     'contactEmail': "undefined@esss.se",
-                     'principalInvestigator': "ESS"
-                     }
-
     def factory(instrument_type=None):
-        if instrument_type == "sonde":
+        if instrument_type == 'sonde':
             return Sonde()
-        if instrument_type == "multiblade":
+        if instrument_type == 'multiblade':
             return Multiblade()
-        if instrument_type == "multigrid":
+        if instrument_type == 'multigrid':
             return Multigrid()
-        if instrument_type == "nmx":
+        if instrument_type == 'nmx':
             return Nmx()
 
     factory = staticmethod(factory)
 
 
+class DefaultInst(Instrument):
+
+    def __init__(self):
+        self.inst = {'owner': 'ESS',
+                     'ownerEmail': 'undefined@esss.se',
+                     'orcidOfOwner': '0000-0000-0000-0000',
+                     'contactEmail': 'undefined@esss.se',
+                     'principalInvestigator': 'ESS',
+                     'userTargetLocation': 'ESS',
+                     'sourceFolder': 'ESS',
+                     'creationLocation': 'ESS',
+                     }
+
+
 class Sonde(Instrument):
 
     def __init__(self):
-        self.inst = {'owner': "Ramsey Al Jebali",
-                     'ownerEmail': "ramsey.aljebali@esss.se",
-                     'orcidOfOwner': "0000-0000-0000-0000",
-                     'contactEmail': "ramsey.aljebali@esss.se",
-                     'principalInvestigator': "Ramsey Al Jebali",
+        self.abbreviation = 'SON'
+        self.inst = {'owner': 'Ramsey Al Jebali',
+                     'ownerEmail': 'ramsey.aljebali@esss.se',
+                     'orcidOfOwner': '0000-0000-0000-0000',
+                     'contactEmail': 'ramsey.aljebali@esss.se',
+                     'principalInvestigator': 'Ramsey Al Jebali',
+                     'userTargetLocation': 'ESS',
+                     'sourceFolder': 'ESS',
+                     'creationLocation': 'ESS',
                      }
 
 
 class Multiblade(Instrument):
 
     def __init__(self):
-        self.inst = {'owner': "Francesco Piscitelli",
-                     'ownerEmail': "Francesco.Piscitelli@esss.se",
-                     'orcidOfOwner': "0000-0002-0325-4407",
-                     'contactEmail': "Francesco.Piscitelli@esss.se",
-                     'principalInvestigator': "Francesco Piscitelli"
+        self.abbreviation = 'MB'
+        self.inst = {'owner': 'Francesco Piscitelli',
+                     'ownerEmail': 'Francesco.Piscitelli@esss.se',
+                     'orcidOfOwner': '0000-0002-0325-4407',
+                     'contactEmail': 'Francesco.Piscitelli@esss.se',
+                     'principalInvestigator': 'Francesco Piscitelli',
+                     'userTargetLocation': 'multiblade',
+                     'sourceFolder': 'multiblade',
+                     'creationLocation': 'multiblade',
                      }
 
 
 class Multigrid(Instrument):
     def __init__(self):
-        self.inst = {'owner': "Anton Khaplanov",
-                     'ownerEmail': "anton.khaplanov@esss.se",
-                     'orcidOfOwner': "0000-0002-8421-1184",
-                     'contactEmail': "anton.khaplanov@esss.se",
-                     'principalInvestigator': "Anton Khaplanov",
+        self.abbreviation = 'MG'
+        self.inst = {'owner': 'Anton Khaplanov',
+                     'ownerEmail': 'anton.khaplanov@esss.se',
+                     'orcidOfOwner': '0000-0002-8421-1184',
+                     'contactEmail': 'anton.khaplanov@esss.se',
+                     'principalInvestigator': 'Anton Khaplanov',
+                     'userTargetLocation': 'multigrid',
+                     'sourceFolder': 'multigrid',
+                     'creationLocation': 'multigrid',
                      }
 
 
 class Nmx(Instrument):
     def __init__(self):
-        self.inst = {'owner': "Dorothea Pfeiffer",
-                     'ownerEmail': "Dorothea.Pfeiffer@esss.se",
-                     'orcidOfOwner': "0000-0003-3893-2308",
-                     'contactEmail': "Dorothea.Pfeiffer@esss.se",
-                     'principalInvestigator': "Dorothea Pfeiffer",
+        self.abbreviation = 'NMX'
+        self.inst = {'owner': 'Dorothea Pfeiffer',
+                     'ownerEmail': 'Dorothea.Pfeiffer@esss.se',
+                     'orcidOfOwner': '0000-0003-3893-2308',
+                     'contactEmail': 'Dorothea.Pfeiffer@esss.se',
+                     'principalInvestigator': 'Dorothea Pfeiffer',
+                     'userTargetLocation': 'NMX',
+                     'sourceFolder': 'NMX',
+                     'creationLocation': 'NMX',
                      }
 
 
 if __name__ == '__main__':
-    sonde = Instrument.factory("sonde")
+    sonde = Instrument.factory('sonde')
     print(sonde.inst)

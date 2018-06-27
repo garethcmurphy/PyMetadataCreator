b387b93249910fa4cfe0fc47e85e03967a3643c4
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-27 10:28:58 +0200

    📚

--- metadatacreator/find_pre.py
+++ metadatacreator/find_pre.py
@@ -6,9 +6,9 @@ import os
 
 root = './'
 files = itertools.chain.from_iterable((
-    files for _,_,files in os.walk(root)
-    ))
+    files for _, _, files in os.walk(root)
+))
 counter = collections.Counter(
     (os.path.basename(file_)[0:10] for file_ in files)
 )
-print (json.dumps(counter, indent=2))
+print(json.dumps(counter, indent=2))

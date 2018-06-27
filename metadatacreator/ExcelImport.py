02cb916c1cdc8d11fabc5a763e40c3d0edc217e2
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-27 10:28:57 +0200

    ✨

--- metadatacreator/ExcelImport.py
+++ metadatacreator/ExcelImport.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
-
-import xlrd
 import pandas as pd
-
 
 
 class ExcelImporter:
     def __init__(self):
         self.filename = "/Users/garethmurphy/Documents/multigrid_metadata.xlsx"
         pass
-    
+
     def read_excel(self) -> object:
-        df_out = pd.read_excel(self.filename,  index_col=None, header=0)
-        #print(df_out)
+        df_out = pd.read_excel(self.filename, index_col=None, header=0)
+        # print(df_out)
         print(list(df_out))
         print(df_out)
 
-        
+
 if __name__ == '__main__':
     ei = ExcelImporter()
     ei.read_excel()

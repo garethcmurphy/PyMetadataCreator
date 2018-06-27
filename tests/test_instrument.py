f55a6e3cfd0c16761f9f6336710fd3018a19619c
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-27 10:56:12 +0200

    ✨

--- tests/test_instrument.py
+++ tests/test_instrument.py
@@ -3,5 +3,5 @@ from ..metadatacreator import instrument
 
 def test_instrument():
     my_instrument = instrument.Instrument()
-    d = my_instrument.ownerEmail
+    d = my_instrument.inst["ownerEmail"]
     assert d == "undefined@esss.se"

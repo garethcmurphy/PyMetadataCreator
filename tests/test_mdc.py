7eb9a9ddc6adb77ac5845d16d9e594ff8652b9ba
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-27 13:00:24 +0200

    ✨

--- tests/test_mdc.py
+++ tests/test_mdc.py
@@ -3,5 +3,5 @@ from ..metadatacreator import PyMetadataCreator
 
 def test_dataset():
     my_dataset = PyMetadataCreator.PyMetadataCreator()
-    d = my_dataset.orig
-    assert d["size"] == 20
+    d = my_dataset.mydir
+    assert d == "./data"

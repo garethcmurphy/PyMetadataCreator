3c5b36e2ba79cadc94dde6b31b03b5eeeaac2715
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-27 10:28:58 +0200

    📇

--- metadatacreator/find_ext.py
+++ metadatacreator/find_ext.py
@@ -8,18 +8,16 @@ from collections import OrderedDict
 
 root = './'
 files = itertools.chain.from_iterable((
-    files for _,_,files in os.walk(root)
-    ))
+    files for _, _, files in os.walk(root)
+))
 counter = collections.Counter(
     (os.path.splitext(file_)[1] for file_ in files)
 )
-#counter_sorted= sorted(counter, key=counter.get)
-#for w in sorted(counter, key=counter.get, reverse=True):
+# counter_sorted= sorted(counter, key=counter.get)
+# for w in sorted(counter, key=counter.get, reverse=True):
 #  print w, counter[w]
 
 
-sc= OrderedDict(sorted(counter.items(), key=lambda t: t[1],  reverse=True))
+sc = OrderedDict(sorted(counter.items(), key=lambda t: t[1], reverse=True))
 
-
-
-print (json.dumps(sc, indent=2))
+print(json.dumps(sc, indent=2))

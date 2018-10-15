a60c5adf75d541ad258f86de98ddd3f0e6c42382
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-16 00:24:00 +0200

    🙈📚

--- metadatacreator/orig.py
+++ metadatacreator/orig.py
@@ -1,20 +1,19 @@
 class Orig:
-    def __init__(self):
-        self.orig = {
-            "size": 20,
-            "dataFileList": "test",
-            "ownerGroup": "ess",
-            "accessGroups": [
-                "brightness",
-                "odin",
-                "loki",
-                "ess"
-            ],
-            "createdBy": "ingestor",
-            "updatedBy": "ingestor",
-            "datasetId": "10.17199/",
-            "rawDatasetId": "string",
-            "derivedDatasetId": "string",
-            "createdAt": "2018-04-23T09:23:47.918Z",
-            "updatedAt": "2018-04-23T09:59:04.506Z"
-        }
+    size = 20
+    dataFileList = "test"
+    ownerGroup = "ess"
+    accessGroups = [
+        "brightness",
+        "odin",
+        "loki",
+        "ess"
+    ]
+    createdBy = "ingestor"
+    updatedBy = "ingestor"
+    datasetId = "10.17199/"
+    rawDatasetId = "string"
+    derivedDatasetId = "string"
+    createdAt = "2018-04-23T09:23:47.918Z"
+    updatedAt = "2018-04-23T09:59:04.506Z"
+
+

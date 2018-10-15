430bf736320e392825d919172a633ad458a77596
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-16 00:35:35 +0200

    🙈📇

--- tests/test_orig.py
+++ tests/test_orig.py
@@ -3,5 +3,5 @@ import metadatacreator.orig
 
 def test_dataset():
     my_dataset = metadatacreator.orig.Orig()
-    d = my_dataset.orig
-    assert d["size"] == 20
+    d = my_dataset.size
+    assert d == 20

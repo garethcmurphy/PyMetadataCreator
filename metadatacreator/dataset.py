979f26b7397ed3f69668614cd343b067941de570
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-16 00:24:00 +0200

    🙈🐛

--- metadatacreator/dataset.py
+++ metadatacreator/dataset.py
@@ -1,65 +1,52 @@
 class PublishedData:
-
-    def __init__(self):
-        self.published_data = {
-            "doi": "string",
-            "affiliation": "string",
-            "creator": "string",
-            "publisher": "string",
-            "publicationYear": 2017,
-            "title": "string",
-            "url": "string",
-            "abstract": "string",
-            "resourceType": "string",
-            "thumbnail": "string",
-            "sizeOfArchive": 25.1,
-            "numberOfFiles": 33,
-            "pidArray": ["string"],
-            "authors": ["string"],
-            "doiRegisteredSuccessfullyTime": "2018-06-07T09:46:27.560Z"
-        }
+    doi = "string"
+    affiliation = "string"
+    creator = "string"
+    publisher = "string"
+    publicationYear = 2017
+    title = "string"
+    url = "string"
+    abstract = "string"
+    resourceType = "string"
+    thumbnail = "string"
+    sizeOfArchive = 25.1
+    numberOfFiles = 33
+    pidArray = ["string"]
+    authors = ["string"]
+    doiRegisteredSuccessfullyTime = "2018-06-07T09:46:27.560Z"
 
 
 class Dataset:
+    principalInvestigator = "Francesco Piscitelli"
+    endTime = "2018-06-07T09:46:27.560Z"
+    creationLocation = "Multiblade"
+    dataFormat = "lst"
+    scientificMetadata = {"run": "1"}
+    owner = "Francesco Piscitelli"
+    ownerEmail = "francesco.piscitelli@esss.se"
+    orcidOfOwner = "0000-0002-0325-4407"
+    contactEmail = "francesco.piscitelli@esss.se"
+    sourceFolder = "multiblade"
+    proposalId = "2018ESS1"
+    size = 102400
+    pid = "MB00001"
+    packedSize = 1024000
+    creationTime = "2018-06-07T08:46:19.611Z"
+    validationStatus = "valid"
+    keywords = ["Vanadium"]
+    description = "string"
+    userTargetLocation = "Multiblade"
+    classification = "analyzed"
+    license = "ESS"
+    version = "v1"
+    type = "raw"
+    doi = "replace with doi"
+    isPublished = True
+    ownerGroup = "ess"
+    accessGroups = ["brightness", "odin", "loki", "ess"]
+    createdBy = "ingestor"
+    updatedBy = "ingestor"
+    createdAt = "2018-06-07T08:46:19.611Z"
+    updatedAt = "2018-06-07T08:46:19.611Z"
 
-    def __init__(self):
-        self.dataset = {
-            "principalInvestigator": "Francesco Piscitelli",
-            "endTime": "2018-06-07T09:46:27.560Z",
-            "creationLocation": "Multiblade",
-            "dataFormat": "lst",
-            "scientificMetadata": {"run": "1"},
-            "owner": "Francesco Piscitelli",
-            "ownerEmail": "francesco.piscitelli@esss.se",
-            "orcidOfOwner": "0000-0002-0325-4407",
-            "contactEmail": "francesco.piscitelli@esss.se",
-            "sourceFolder": "multiblade",
-            "proposalId": "2018ESS1",
-            "size": 102400,
-            "pid": "MB00001",
-            "packedSize": 1024000,
-            "creationTime": "2018-06-07T08:46:19.611Z",
-            "validationStatus": "valid",
-            "keywords": [
-                "Vanadium"
-            ],
-            "description": "string",
-            "userTargetLocation": "Multiblade",
-            "classification": "analyzed",
-            "license": "ESS",
-            "version": "v1",
-            "type": "raw",
-            "doi": "replace with doi",
-            "isPublished": True,
-            "ownerGroup": "ess",
-            "accessGroups": [
-                "brightness",
-                "odin",
-                "loki",
-                "ess"
-            ],
-            "createdBy": "ingestor",
-            "updatedBy": "ingestor",
-            "createdAt": "2018-06-07T08:46:19.611Z",
-            "updatedAt": "2018-06-07T08:46:19.611Z"
-        }
+

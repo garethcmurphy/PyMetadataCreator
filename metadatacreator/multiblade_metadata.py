b5f7ebe5c453cd3a26c1ad98c84a215d344e8b3e
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-16 15:43:48 +0200

    🐛

--- metadatacreator/multiblade_metadata.py
+++ metadatacreator/multiblade_metadata.py
@@ -1,5 +1,91 @@
 class MultibladeMetadata:
     metadata_object = {
-        '0001': {"wavelength": 2},
-        '0048': {"wavelength": 2}
+        '0001': {"wavelength": 2, 'comment': 'xxx'},
+        '0011': {'tag': '03_2_A_Ni',
+                 'comment': 'reflected Beam Ni: Measurements of Ni -> magnetic (double fringe)'},
+        '0012': {'tag': '03_3_B_DirectBeam', 'comment': 'direct Beam Measurements',
+                 'current Total microA': '1',
+                 'time ': '2 minutes',
+                 'fileNumber': '43',
+                 'start': '167.185',
+                 'stop': '166',
+                 },
+        '0012': {
+            'tag': '03_4_B_DirectBeamTests',
+            'comment': 'reflected Beam Ni: Measurements of Ni -> magnetic (double fringe)'
+        },
+        '0014': {
+            'tag': '03_5_C_DirectBeam_5mmAl',
+            'comment': 'direct Beam, 5 minutes Al plate in front of the window',
+            'time': '2 minutes',
+            'start': '167/2.82',
+            'stop': '166.6/25.92',
+        },
+        '0015': {'tag': '03_6_D_Ir_align',
+                 'comment': 'xxx'
+                 },
+        '0016': {'tag': '03_7_E_Ir_collimated',
+                 'comment': 'xxx'
+                 },
+        '0017': {'tag': '03_8_E_Ir_slit', 'comment': 'xxx'},
+        '0018': {'tag': '03_9_E_Ir_test', 'comment': 'xxx'},
+        '0019': {'tag': '03N_12_G_plexi', 'comment': 'xxx'},
+        '0020': {
+            'tag': '04_10_I2_Si_divergent', 'comment': 'xxx'},
+        '0021': {'tag': '04_11_L_FeSi_test', 'comment': 'xxx'},
+        '0022': {'tag': '04_1_E_Ir_test', 'comment': 'xxx'},
+        '0023': {
+            'tag': '04_2_E1_Ir_collimated', 'comment': 'xxx'},
+        '0024': {'tag': '04_3_H_Si_test', 'comment': 'xxx'},
+        '0025': {'tag': '04_4_H_Si_collimated', 'comment': 'xxx'},
+        '0026': {
+            'tag': '04_5_H1_Si_collimated_HVtests', 'comment': 'xxx'},
+        '0027': {'tag': '04_6_I_FeSi_test', 'comment': 'xxx'},
+        '0028': {
+            'tag': '04_7_I_FeSi_divergent',
+            'comment': 'sample supermirror divergent'},
+        '0029': {
+            'tag': '04_8_I1_FeSi_divergent_magnet', 'comment': 'xxx'},
+        '0030': {
+            'tag': '04_9_I2_Si_divergent_test', 'comment': 'xxx'},
+        '0031': {
+            'tag': '04N_12_L_FeSi_collimated_OffSpec',
+            'comment': 'Off Specular (FeSi) m=3.8 collimated beam'
+        },
+        '0032': {'tag': '05_10_P_mask',
+                 'comment': 'xxx'},
+        '0033': {
+            'tag': '05_11_Q_Si_divergent_highQ_test',
+            'comment': 'xxx'},
+        '0034': {
+            'tag': '05_1_M_FeSi_collimated_magnet_test',
+            'comment': 'xxx'},
+        '0035': {
+            'tag': '05_2_M_FeSi_collimated_magnet',
+            'comment': 'xxx'},
+        '0036': {'tag': '05_3_N_ResW_test', 'comment': 'xxx'},
+        '0037': {'tag': '05_4_N_ResW', 'comment': 'xxx'},
+        '0038': {'tag': '05_5_N1_ResW_longRun', 'comment': 'xxx'},
+        '0039': {'tag': '05_6_O_PosStrip_test', 'comment': 'xxx'},
+        '0040': {
+            'tag': '05_7_O_PosStrip_linkN1', 'comment': 'xxx'},
+        '0041': {'tag': '05_8_O1_ResStrip', 'comment': 'xxx'},
+        '0042': {'tag': '05_9_P_mask_test',
+                 'comment': 'resolution mask',
+                 'comment2': '''first direct beam on detector. To have a wider beam we put the 
+                 supermirror with the magnet and divergent beam. We use the ESS mask''',
+                 },
+        '0043': {
+            'tag': '05N_12_Q_Si_divergent_highQ', 'comment': 'xxx'},
+        '0044': {
+            'tag': '05N_13_Q_Si_divergent_highQ_BeamOff', 'comment': 'xxx'},
+        '0045': {
+            'tag': '06_1_R_Efficiency_50Hz_test', 'comment': 'xxx'},
+        '0046': {
+            'tag': '06_2_R_Efficiency_50Hz', 'comment': 'xxx'},
+        '0047': {
+            'tag': '06_3_R12_Efficiency_25Hz_test', 'comment': 'xxx'},
+        '0048': {
+            'tag': '06_4_R2_Efficiency_25Hz'
+        }
     }

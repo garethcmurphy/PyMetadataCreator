dca4dabd96ec2dc935f9023909f5b3c89cfd70a7
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-16 09:24:57 +0200

    🐎

--- tests/context.py
+++ tests/context.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from metadatacreator import dataset
 from metadatacreator import instrument
-from metadatacreator import orig
+from metadatacreator import origdatablocks
 
 
 

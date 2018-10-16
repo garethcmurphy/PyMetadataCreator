71c1bab3234c7579ffdd21ced2956f7aac478bd1
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-16 09:24:56 +0200

    🐛

--- metadatacreator/dataset.py
+++ metadatacreator/dataset.py
@@ -22,16 +22,16 @@ class Dataset:
     creationLocation = "Multiblade"
     dataFormat = "lst"
     scientificMetadata = {"run": "1"}
+    pid = "MB00001"
     owner = "Francesco Piscitelli"
     ownerEmail = "francesco.piscitelli@esss.se"
     orcidOfOwner = "0000-0002-0325-4407"
     contactEmail = "francesco.piscitelli@esss.se"
     sourceFolder = "multiblade"
-    proposalId = "2018ESS1"
     size = 102400
-    pid = "MB00001"
     packedSize = 1024000
     creationTime = "2018-06-07T08:46:19.611Z"
+    type = "raw"
     validationStatus = "valid"
     keywords = ["Vanadium"]
     description = "string"
@@ -39,7 +39,6 @@ class Dataset:
     classification = "analyzed"
     license = "ESS"
     version = "v1"
-    type = "raw"
     doi = "replace with doi"
     isPublished = True
     ownerGroup = "ess"
@@ -48,5 +47,7 @@ class Dataset:
     updatedBy = "ingestor"
     createdAt = "2018-06-07T08:46:19.611Z"
     updatedAt = "2018-06-07T08:46:19.611Z"
+    sampleId = "2018ESS1"
+    proposalId = "2018ESS1"
 
 

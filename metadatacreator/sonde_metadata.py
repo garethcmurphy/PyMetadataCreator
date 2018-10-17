4e3bfa7a6808b5d3626f8983944e64ddedd2a8f5
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-17 23:09:22 +0200

    reading sonde metadata

--- metadatacreator/sonde_metadata.py
+++ metadatacreator/sonde_metadata.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python3
+import json
+
+
 class SondeMetadata:
-    metadata_object = {
-        '0001': {"wavelength": 2,
-                 'comment': 'SONDE'},
-    }
+
+    def __init__(self):
+        with open('sonde.json') as json_data:
+            self.metadata_object = json.load(json_data)
+
+
+if __name__ == '__main__':
+    nmx = SondeMetadata()
+    print(nmx.metadata_object)
